9a01951cfa6a6b43e9d4a795cad27a74ced972d4
Kwon KiHoon <inyourcareallen@gmail.com>
2024-04-12 13:49:24 +0900

    drawing lines

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
 // initial cofiguration
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 15);
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+//// cube
+const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+//// line
+//create a blue LineBasicMaterial
+const line = new THREE.Line(new THREE.BufferGeometry()
+  .setFromPoints([new THREE.Vector3(- 10, 0, 0), new THREE.Vector3(0, 10, 0), new THREE.Vector3(10, 0, 0)]), new THREE.LineBasicMaterial({ color: 0x0000ff }));
 
-camera.position.z = 5;
-// renderer.render( scene, camera );
-
+// functions
 function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
-animate();
+
+
+// rendering
+if (WebGL.isWebGLAvailable()) {
+
+  // Initiate function or other initializations here
+  scene.add(cube);
+  scene.add(line);
+  renderer.render(scene, camera);
+  // animate();
+
+} else {
+
+  const warning = WebGL.getWebGLErrorMessage();
+  document.getElementById('container').appendChild(warning);
+
+}
